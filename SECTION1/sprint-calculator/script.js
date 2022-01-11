380d3973f8cf00ce29e558c33acd7c4e2ce84789
yjy1412 <yjy852@gmail.com>
2022-01-11 13:38:54 +0900

    feat: 스프린트-계산기 미니멈 테스트 통과

--- SECTION1/sprint-calculator/script.js
+++ SECTION1/sprint-calculator/script.js
@@ -10,28 +10,45 @@ function calculate(n1, operator, n2) {
   let result = 0;
   // TODO : n1과 n2를 operator에 따라 계산하는 함수를 만드세요.
   // ex) 입력값이 n1 : '1', operator : '+', n2 : '2' 인 경우, 3이 리턴됩니다.
+  if (operator === '+') {
+    result = n1 + n2;
+  } else if (operator === '-') {
+    result = n1 - n2;
+  } else if (operator === '*') {
+    result = n1 * n2;
+  } else if (operator === '/') {
+    result = n1 / n2;
+  }
+  console.log(result);
   return String(result);
 }
 
 buttons.addEventListener('click', function (event) {
   // 버튼을 눌렀을 때 작동하는 함수입니다.
-
   const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
+  console.log("target is", target);
   const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
   const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
   // ! 위 코드(Line 19 - 21)는 수정하지 마세요.
 
   if (target.matches('button')) {
     // TODO : 계산기가 작동할 수 있도록 아래 코드를 수정하세요. 작성되어 있는 조건문과 console.log를 활용하시면 쉽게 문제를 풀 수 있습니다.
+
     // 클릭된 HTML 엘리먼트가 button이면
     if (action === 'number') {
       // 그리고 버튼의 클레스가 number이면
       // 아래 코드가 작동됩니다.
       console.log('숫자 ' + buttonContent + ' 버튼');
+      if ( firstOperend.textContent === '0' ) {
+        firstOperend.textContent = buttonContent;
+      } else {
+        secondOperend.textContent = buttonContent;
+      }
     }
 
     if (action === 'operator') {
       console.log('연산자 ' + buttonContent + ' 버튼');
+      operator.textContent = buttonContent;
     }
 
     if (action === 'decimal') {
@@ -40,10 +57,19 @@ buttons.addEventListener('click', function (event) {
 
     if (action === 'clear') {
       console.log('초기화 버튼');
+      firstOperend.textContent = '0';
+      operator.textContent = '+';
+      secondOperend.textContent = '0';
+      calculatedResult.textContent = '0';
     }
 
     if (action === 'calculate') {
       console.log('계산 버튼');
+      const firstNum = Number(firstOperend.textContent);
+      const operatorText = operator.textContent;
+      const secondNum = Number(secondOperend.textContent);
+
+      calculatedResult.textContent = calculate(firstNum, operatorText, secondNum);
     }
   }
 });
@@ -64,11 +90,11 @@ buttons.addEventListener('click', function (event) {
 
   // ! 여기서부터 Advanced Challenge & Nightmare 과제룰 풀어주세요.
   if (target.matches('button')) {
-    if (action === 'number') {}
-    if (action === 'operator') {}
-    if (action === 'decimal') {}
-    if (action === 'clear') {}
-    if (action === 'calculate') {}
+    if (action === 'number') { }
+    if (action === 'operator') { }
+    if (action === 'decimal') { }
+    if (action === 'clear') { }
+    if (action === 'calculate') { }
   }
 
 });
